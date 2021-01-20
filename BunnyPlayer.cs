8936b189e7f1cbd0b2ea1dd49236f655fd2fb979
Mikadun <gribak98@list.ru>
2021-01-20 18:22:42 +1000

    Добавьте файлы проекта.

---
+++ BunnyPlayer.cs
@@ -0,0 +1,55 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+using Terraria.ID;
+using System.Collections.Generic;
+using BunnyMod.Mounts.Transformations;
+
+namespace BunnyMod
+{
+    public class BunnyModPlayer : ModPlayer
+    {
+        public bool transformation = true;
+
+        public override void OnRespawn(Player player)
+        {
+            player.mount.SetMount(ModContent.MountType<SquirrelTransformation>(), player, false);
+        }
+
+        public override void OnEnterWorld(Player player)
+        {
+            OnRespawn(player);
+        }
+
+        private void ReserEffects()
+        {
+            transformation = true;
+        }
+
+        public override void Initialize()
+        {
+            //OnRespawn(player);
+        }
+
+        public override void ModifyDrawLayers(List<PlayerLayer> layers)
+        {
+            // if transformed make invisible all layers except mount and misc effects
+            if (transformation)
+            {
+                foreach (PlayerLayer layer in layers)
+                {
+                    if (layer != PlayerLayer.MountBack 
+                     && layer != PlayerLayer.MountFront 
+                     && layer != PlayerLayer.MiscEffectsFront 
+                     && layer != PlayerLayer.MiscEffectsBack)
+                    {
+                        layer.visible = false;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Mounts/Transformations/SquirrelTransformation.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BunnyMod.Mounts.Transformations
+{
+    public class SquirrelTransformation : ModMountData
+    {
+        public override void SetDefaults()
+        {
+            Mount.MountData mount = this.mountData;
+            mount.buff = BuffID.Regeneration;
+            mount.heightBoost = -26;
+            mount.fallDamage = 0.25f;
+            mount.runSpeed = 4.0f;
+            mount.dashSpeed = 5.0f;
+            mount.flightTimeMax = 0;
+            mount.fatigueMax = 0;
+            mount.jumpHeight = 12;
+            mount.acceleration = 0.25f;
+            mount.jumpSpeed = 4.0f;
+            mount.blockExtraJumps = false;
+            mount.totalFrames = 6;
+            mount.constantJump = false;
+            int[] numArray = new int[mount.totalFrames];
+            for (int index = 0; index < numArray.Length; ++index)
+                numArray[index] = 0;
+            mount.playerYOffsets = numArray;
+            mount.xOffset = 0;
+            mount.bodyFrame = 0;
+            mount.yOffset = -4;
+            mount.playerHeadOffset = -24;
+            mount.standingFrameCount = 1;
+            mount.standingFrameDelay = 10;
+            mount.standingFrameStart = 0;
+            mount.runningFrameCount = 5;
+            mount.runningFrameDelay = 15;
+            mount.runningFrameStart = 1;
+            mount.flyingFrameCount = 1;
+            mount.flyingFrameDelay = 0;
+            mount.flyingFrameStart = 2;
+            mount.inAirFrameCount = 1;
+            mount.inAirFrameDelay = 0;
+            mount.inAirFrameStart = 2;
+            mount.idleFrameCount = 1;
+            mount.idleFrameDelay = 10;
+            mount.idleFrameStart = 0;
+            mount.idleFrameLoop = true;
+            mount.swimFrameCount = mount.inAirFrameCount;
+            mount.swimFrameDelay = mount.inAirFrameDelay;
+            mount.swimFrameStart = mount.inAirFrameStart;
+            if (Main.netMode == NetmodeID.Server)
+                return;
+            mount.textureWidth = mount.backTexture.Width;
+            mount.textureHeight = mount.backTexture.Height;
+        }
+
+        public SquirrelTransformation() : base()
+        {
+            return;
+        }
+    }
+}
