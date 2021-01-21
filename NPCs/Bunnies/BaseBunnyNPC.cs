d00dece3aad9f8a9533420bb930be37713e95ea1
Mikadun <gribak98@list.ru>
2021-01-22 01:08:27 +1000

    Add basic base bunny class and icon for mod

---
+++ NPCs/Bunnies/BaseBunnyNPC.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BunnyMod.NPCs.Bunnies
+{
+    public abstract class BaseBunnyNPC : ModNPC
+    {
+        public override void SetDefaults()
+        {
+            npc.CloneDefaults(NPCID.Bunny);
+            Main.npcFrameCount[npc.type] = Main.npcFrameCount[NPCID.Bunny];
+            animationType = NPCID.Bunny;
+        }
+    }
+}
